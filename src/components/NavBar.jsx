7045a15e162b4bc22830b553a6e835bfbcf6ea96
harshilnashier <93786527+harshilnashier@users.noreply.github.com>
2024-07-30 16:56:44 +0530

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,7 @@ function NavBar() {
   return (
     <div className=" flex items-center space-x-8 text-black-400 text-xl font-bold pl-12 py-4"
     >
-        <img src={Logo} className="w-[50px]"></img>
+        <img src={Logo} alt="" className="w-[50px]"></img>
         <Link to="/">Movies</Link>
         <Link to="/favourites">Favourites</Link>
     </div>
